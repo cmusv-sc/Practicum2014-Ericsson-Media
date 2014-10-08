45349fc45ea88680a41f719c0c1721958c25dd78
Jeremy Fu <gengf@andrew.cmu.edu>
2014-10-08 13:17:46 -0700

    Review the edu.cmu.messagebus.WebClient class

--- src/main/java/edu/cmu/messagebus/WebClient.java
+++ src/main/java/edu/cmu/messagebus/WebClient.java
@@ -20,25 +20,9 @@ import edu.cmu.messagebus.message.WebClientUpdateMessage.Node;
 public class WebClient {
 	private static String js;  
 	
+	//TODO: Do we need use ConcurrentHashMap? REVIEWED BY JEREMY
 	private Map<String, WebClientUpdateMessage.Node> nodes = new HashMap<String, WebClientUpdateMessage.Node>();
-	/**
-	 * 
-	 * @param resourceName path relative to resources folder like "js\abc.js"
-	 * @return byte[]
-	 */
-	public static byte[] getResourceBytes(String resourceName){
-		InputStream is = WebClient.class.getClassLoader().getResourceAsStream(resourceName);
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		byte[] buf = new byte[4096];				
-		int read;				
-		try {
-			while ((read = is.read(buf)) > -1)
-				bos.write(buf, 0, read);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return bos.toByteArray();
-	}
+	
 	private static TrapHostable mainClientPage = new TrapHostable("text/html") {
 		@Override
 		public byte[] getBytes() {
@@ -71,20 +55,45 @@ public class WebClient {
 			return getResourceBytes("js\\sigma.layout.forceAtlas2.min.js");
 		}
 	};
+	
+	/**
+	 * 
+	 * @param resourceName path relative to resources folder like "js\abc.js"
+	 * @return byte[]
+	 */
+	
+	//TODO: Is this method going to be exposed? REVIEWED BY: JEREMY
+	public static byte[] getResourceBytes(String resourceName){
+		InputStream is = WebClient.class.getClassLoader().getResourceAsStream(resourceName);
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		byte[] buf = new byte[4096];				
+		int read;				
+		try {
+			while ((read = is.read(buf)) > -1)
+				bos.write(buf, 0, read);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return bos.toByteArray();
+	}
 
 	public void load(WarpDomain domain) throws WarpException, IOException, TrapException{
 		domain.getJSLibraryURI(true);
 		js = domain.getEmbeddedJSWithAuthToken(true, true);
+		
+		//TODO: purpose of printing out the information? REVIEWED BY JEREMY
 		System.out.println(domain.addHostedObject(mainClientPage, "index.html"));
 		System.out.println(domain.addHostedObject(displayGraphJs, "js/displayGraph.js"));
 		System.out.println(domain.addHostedObject(sigmaJs, "js/sigma.min.js"));
 		System.out.println(domain.addHostedObject(sigmaJsonParserJs, "js/sigma.parsers.json.min.js"));
 		System.out.println(domain.addHostedObject(sigmaForceAtlas2Js, "js/sigma.layout.forceAtlas2.min.js"));
+	
 	}
 
 	public void addNode(WebClientUpdateMessage.Node newNode){
 		this.nodes.put(newNode.id, newNode);
 	}
+	
 	public void removeNode(WebClientUpdateMessage.Node node){
 		this.nodes.remove(node.id);
 	}
