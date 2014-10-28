fe9af4df60457c0bb2550e1f5db5876a56c78411
Jeremy Fu <gengf@andrew.cmu.edu>
2014-10-28 13:44:17 -0700

    Add comments.

--- src/main/java/edu/cmu/mdnsim/config/StreamSpec.java
+++ src/main/java/edu/cmu/mdnsim/config/StreamSpec.java
@@ -8,5 +8,10 @@ public class StreamSpec extends MbMessage {
 	public String StreamId;
 	public String DataSize;
 	public String ByteRate;
+	
+	/**
+	 * The flow list is ordered from sink -> middle nodes -> source
+	 * The first Map in the flow is assumed to be the sink
+	 */
 	public ArrayList<HashMap<String, String>> Flow;
 }
