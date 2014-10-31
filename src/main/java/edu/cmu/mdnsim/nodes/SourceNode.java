492b327a1f851844aab217692b92bca9e2206b9a
Jeremy Fu <gengf@andrew.cmu.edu>
2014-10-31 00:25:59 -0700

    Implement terminateTask() and releaseResource() methods in SourceNode. *terminateTask() stops sending data to sink. *releaseResource() closes sockets. This method is called by terminateTask() directly without using methodListner at resource NodeId/tasks#DELETE

--- src/main/java/edu/cmu/mdnsim/nodes/SourceNode.java
+++ src/main/java/edu/cmu/mdnsim/nodes/SourceNode.java
@@ -7,6 +7,7 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -21,30 +22,90 @@ import edu.cmu.util.Utility;
 
 public class SourceNode extends AbstractNode {
 	
+	/**
+	 * This instance variable is used to control whether print out info which 
+	 * is used in Hao's unit test.
+	 */
+	private boolean UNIT_TEST = false;
+	
+	
+	private Map<String, SendThread> runningMap = new HashMap<String, SendThread>();
+	
 	public SourceNode() throws UnknownHostException {
 		super();
 	}	
 	
-	/**
-	 * 
-	 * Starts to send data to specified destination.
-	 * 
-	 * @param streamId The stream id associated with this stream
-	 * @param destAddrStr The destination address
-	 * @param destPort The destination port
-	 * @param bytesToTransfer Total bytes to be sent
-	 * @param rate The rate to send data
-	 * 
-	 */
-	private void sendAndReport(String streamId, InetAddress destAddrStr, int destPort, int bytesToTransfer, int rate) {
-		WarpThreadPool.executeCached(new SendThread(streamId, destAddrStr, destPort, bytesToTransfer, rate));
-	}
 	
 	public void sendAndReportTest(String streamId, InetAddress destAddrStr, int destPort, int bytesToTransfer, int rate){
 		ExecutorService executorService = Executors.newCachedThreadPool();
 		executorService.execute(new SendThread(streamId, destAddrStr, destPort, bytesToTransfer, rate));
 	}
 	
+	@Override
+	public void executeTask(StreamSpec streamSpec) {
+
+		if (ClusterConfig.DEBUG) {
+			System.out.println("[DEBUG]SourceNode.executeTask(): Source received a work specification.");
+		}
+		for (HashMap<String, String> currentFlow : streamSpec.Flow) {
+
+			if (currentFlow.get("NodeId").equals(getNodeName())) {
+				String[] ipAndPort = currentFlow.get("ReceiverIpPort").split(":");
+				String destAddrStr = ipAndPort[0];
+				int destPort = Integer.parseInt(ipAndPort[1]);
+				int dataSize = Integer.parseInt(streamSpec.DataSize);
+				int rate = Integer.parseInt(streamSpec.ByteRate);
+				try {
+					SendThread sndThread = new SendThread(streamSpec.StreamId, InetAddress.getByName(destAddrStr), destPort, dataSize, rate);
+					runningMap.put(streamSpec.StreamId, sndThread);
+					WarpThreadPool.executeCached(sndThread);
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				}
+				break;
+			}
+		}
+	}
+
+	@Override
+	public void terminateTask(StreamSpec streamSpec) {
+		
+		if (ClusterConfig.DEBUG) {
+			System.out.println("[DEBUG]SourceNode.terminateTask(): Source received terminate task.\n" + JSON.toJSON(streamSpec));
+		}
+		
+		SendThread sndThread = runningMap.get(streamSpec.StreamId);
+		sndThread.kill();
+		
+		releaseResource(streamSpec);
+	}
+	
+	@Override
+	public void releaseResource(StreamSpec streamSpec) {
+		
+		SendThread sndThread = runningMap.get(streamSpec.StreamId);
+		while (!sndThread.isStopped());
+		if (ClusterConfig.DEBUG) {
+			System.out.println("[DEBUG]SourceNode.releaseResource(): Source starts to clean-up resource.");
+		}
+		
+		sndThread.clean();
+		runningMap.remove(streamSpec.StreamId);
+		
+		for (int i = 0; i < streamSpec.Flow.size(); i++) {
+			Map<String, String> nodeMap = streamSpec.Flow.get(i);
+			if (nodeMap.get("NodeId").equals(getNodeName())) {
+				
+				try {
+					msgBusClient.send("/tasks", nodeMap.get("DownstreamUri") + "/tasks", "DELETE", streamSpec);
+				} catch (MessageBusException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		}
+		
+	}
 	
 	private class SendThread implements Runnable {
 		
@@ -53,7 +114,11 @@ public class SourceNode extends AbstractNode {
 		private int dstPort;
 		private int bytesToTransfer;
 		private int rate;
-
+		private DatagramSocket sourceSocket = null;
+		
+		private boolean killed = false;
+		private boolean stopped = false;
+		
 		public SendThread(String streamId, InetAddress dstAddrStr, int dstPort, int bytesToTransfer, int rate) {
 			
 			SendThread.this.streamId = streamId;
@@ -77,7 +142,9 @@ public class SourceNode extends AbstractNode {
 			double packetPerSecond = rate / STD_DATAGRAM_SIZE;
 			long millisecondPerPacket = (long)(1 * edu.cmu.mdnsim.nodes.AbstractNode.MILLISECONDS_PER_SECOND / packetPerSecond); 
 			
-			DatagramSocket sourceSocket = null;
+			boolean finished = false;
+			
+			
 			try {
 				sourceSocket = new DatagramSocket();
 			} catch (SocketException socketException) {
@@ -85,10 +152,10 @@ public class SourceNode extends AbstractNode {
 			}
 			
 			report();
-
 			
 			byte[] buf = null;
-			while (bytesToTransfer > 0) {
+			
+			while (!finished && !isKilled()) {
 				
 				long begin = System.currentTimeMillis();
 				
@@ -103,33 +170,54 @@ public class SourceNode extends AbstractNode {
 					ioe.printStackTrace();
 				}
 				bytesToTransfer -= packet.getLength();
-				System.out.println("[Source] " + bytesToTransfer + " " + currentTime());
+				if (UNIT_TEST) {
+					System.out.println("[Source] " + bytesToTransfer + " " + currentTime());
+				}
 				
 				long end = System.currentTimeMillis();
+				
+				finished = (bytesToTransfer <= 0);
+				
 				long millisRemaining = millisecondPerPacket - (end - begin);
+				
 				if (millisRemaining > 0) {
 					try {
 						Thread.sleep(millisRemaining);
 					} catch (InterruptedException ie) {
 						ie.printStackTrace();
 					}
-				}	
-			} 
-			System.out.println("[Source] finish sending");
-			sourceSocket.close();
-			if (ClusterConfig.DEBUG) {
-				if (bytesToTransfer <= 0) {
-					System.out.println("[DEBUG]SourceNode.SendDataThread.run():"
-							+ " This thread has been stopped(not finished yet).");
-				} else {
-					System.out.println("[DEBUG]SourceNode.SendDataThread.run():"
-							+ " This thread hass benn terminated(finished).");
 				}
-			} 
+				
+			}
+
+			
+			
+			if (ClusterConfig.DEBUG) {
+				if (finished) {
+					System.out.println("[DEBUG]SourceNode.SendDataThread.run():"
+							+ " This thread has finished.");
+				} else if (isKilled()){
+					System.out.println("[DEBUG]SourceNode.SendDataThread.run():"
+							+ " This thread has been killed(not finished yet).");
+				}
+			}
+			
+			stop();
+			
+		}
+		
+		/**
+		 * Clean up all resources for this thread.
+		 */
+		public void clean() {
+			sourceSocket.close();
 		}
 		
 		private void report(){
-			System.out.println("[INFO] SourceNode.SendDataThread.run(): " + "Source will start sending data. " + "Record satrt time and report to master");
+			
+			if (ClusterConfig.DEBUG) {
+				System.out.println("[DEBUG] SourceNode.SendDataThread.run(): " + "Source will start sending data. " + "Record satrt time and report to master");
+			}
 			SourceReportMessage srcReportMsg = new SourceReportMessage();
 			srcReportMsg.setStreamId(streamId);
 			srcReportMsg.setTotalBytesTransferred(bytesToTransfer);
@@ -141,35 +229,24 @@ public class SourceNode extends AbstractNode {
 				e.printStackTrace();
 			};
 		}
+		
+		private synchronized void kill() {
+			killed = true;
+		}
+		
+		private synchronized boolean isKilled() {
+			return killed;
+		}
+		
+		private synchronized void stop() {
+			stopped = true;
+		}
+		
+		private synchronized boolean isStopped() {
+			return stopped;
+		}
 	}
 
 
-	@Override
-	public void executeTask(StreamSpec streamSpec) {
-		int flowIndex = -1;
-		System.out.println("Sink received a work specification: "+JSON.toJSON(streamSpec));
-		for (HashMap<String, String> currentFlow : streamSpec.Flow) {
-			flowIndex++;
-			if (!currentFlow.get("NodeId").equals(getNodeName()))
-				continue;
-			else {
-				//System.out.println("FOUND ME!! "+currentFlow.get("NodeId"));
-				String[] ipAndPort = currentFlow.get("ReceiverIpPort").split(":");
-				String destAddrStr = ipAndPort[0];
-				int destPort = Integer.parseInt(ipAndPort[1]);
-				int dataSize = Integer.parseInt(streamSpec.DataSize);
-				int rate = Integer.parseInt(streamSpec.ByteRate);
-				try {
-					sendAndReport(streamSpec.StreamId, InetAddress.getByName(destAddrStr), destPort, dataSize, rate);
-					if (currentFlow.get("UpstreamUri") != null)
-						msgBusClient.send("/tasks", currentFlow.get("UpstreamUri")+"/tasks", "PUT", streamSpec);
-				} catch (MessageBusException e) {
-					e.printStackTrace();
-				} catch (UnknownHostException e) {
-					e.printStackTrace();
-				}
-				break;
-			}
-		}
-	}	
+	
 }
