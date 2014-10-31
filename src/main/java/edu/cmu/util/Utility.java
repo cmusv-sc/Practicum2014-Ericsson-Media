898475043aa92f3f7f7c68855474278d6fdcab6b
Jeremy Fu <gengf@andrew.cmu.edu>
2014-10-31 15:41:39 -0700

    Add stringToMilliseconTime() to Utility.

--- src/main/java/edu/cmu/util/Utility.java
+++ src/main/java/edu/cmu/util/Utility.java
@@ -1,6 +1,7 @@
 package edu.cmu.util;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -8,6 +9,7 @@ import java.util.Locale;
 public class Utility {
 
 	private static DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS", Locale.US);
+	
 	public static String currentTime(){
 		Date date = new Date();
 		return dateFormat.format(date);
@@ -19,4 +21,7 @@ public class Utility {
 		return dateFormat.format(date);
 	}
 	
+	public static long stringToMillisecondTime(String time) throws ParseException {
+		return dateFormat.parse(time).getTime();
+	}
 }
