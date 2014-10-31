0e75ce106460e4ddccb6ca6cae0b3ef31e6600a9
Jeremy Fu <gengf@andrew.cmu.edu>
2014-10-31 15:39:27 -0700

    Comment out all fields MbMessage MbMessage is used as a marker for all control messages. Previous abstraction didn't properlly represent needs for all control messages.

--- src/main/java/edu/cmu/mdnsim/messagebus/message/MbMessage.java
+++ src/main/java/edu/cmu/mdnsim/messagebus/message/MbMessage.java
@@ -2,33 +2,35 @@ package edu.cmu.mdnsim.messagebus.message;
 
 public class MbMessage {
 	
-	protected String streamId;
-	protected int totalBytes;
-	protected String startTime;
-	protected String endTime;
+//	protected String streamId;
+//	protected int totalBytes;
+//	protected String startTime;
+//	protected String endTime;
+//	
+//	public String getStreamId() {
+//		return streamId;
+//	}
+//	public void setStreamId(String streamId) {
+//		this.streamId = streamId;
+//	}
+//	public int getTotalBytes() {
+//		return totalBytes;
+//	}
+//	public void setTotalBytes(int totalBytes) {
+//		this.totalBytes = totalBytes;
+//	}
+//	public String getStartTime() {
+//		return startTime;
+//	}
+//	public void setStartTime(String startTime) {
+//		this.startTime = startTime;
+//	}
+//	public String getEndTime() {
+//		return endTime;
+//	}
+//	public void setEndTime(String endTime) {
+//		this.endTime = endTime;
+//	}
 	
-	public String getStreamId() {
-		return streamId;
-	}
-	public void setStreamId(String streamId) {
-		this.streamId = streamId;
-	}
-	public int getTotalBytes() {
-		return totalBytes;
-	}
-	public void setTotalBytes(int totalBytes) {
-		this.totalBytes = totalBytes;
-	}
-	public String getStartTime() {
-		return startTime;
-	}
-	public void setStartTime(String startTime) {
-		this.startTime = startTime;
-	}
-	public String getEndTime() {
-		return endTime;
-	}
-	public void setEndTime(String endTime) {
-		this.endTime = endTime;
-	}
+	
 }
