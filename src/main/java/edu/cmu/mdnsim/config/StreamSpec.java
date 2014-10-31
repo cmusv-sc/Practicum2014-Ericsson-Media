c84df8fa0b832c10b30598c414c6dc23c0e4ad49
Jeremy Fu <gengf@andrew.cmu.edu>
2014-10-31 00:49:55 -0700

    Change the name of getSinkNodeURI() to findSinkNodeURI(). Because the JSON.toJSON() helper method will treat getSinkNodeURI() as a getter for an instance variable.

--- src/main/java/edu/cmu/mdnsim/config/StreamSpec.java
+++ src/main/java/edu/cmu/mdnsim/config/StreamSpec.java
@@ -1,8 +1,11 @@
 package edu.cmu.mdnsim.config;
 
-import edu.cmu.mdnsim.messagebus.message.MbMessage;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+
+import edu.cmu.mdnsim.messagebus.exception.MessageBusException;
+import edu.cmu.mdnsim.messagebus.message.MbMessage;
 
 public class StreamSpec extends MbMessage {
 	public String StreamId;
@@ -14,4 +17,22 @@ public class StreamSpec extends MbMessage {
 	 * The first Map in the flow is assumed to be the sink
 	 */
 	public ArrayList<HashMap<String, String>> Flow;
+	
+	/**
+	 * This method is a helper method to retrieve URI of the sink node in the flow.
+	 * @return The URI of sink node.
+	 * @throws MessageBusException
+	 */
+	public String findSinkNodeURI() throws MessageBusException {
+		Map<String, String>sinkNodeMap = Flow.get(0);
+		if (sinkNodeMap.get("DownstreamId") != null) {
+			throw new MessageBusException("StreamSpec is mis-formatted. "
+					+ "The first node map is not for sink node.");
+		} else if (sinkNodeMap.get("NodeUri") == null) {
+			throw new MessageBusException("StreamSpec is mis-formatted. "
+					+ "The first node map doesn't contain field NodeUri");
+		} else {
+			return sinkNodeMap.get("NodeUri");
+		}
+	}
 }
