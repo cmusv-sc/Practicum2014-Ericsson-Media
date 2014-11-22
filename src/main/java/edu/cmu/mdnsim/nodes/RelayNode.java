53e7372f8bba98581ebaaa5bdc073dfaf88e67ea
Jigar Patel <jigarbjpatel@gmail.com>
2014-11-21 23:00:42 -0800

    Added new test input files

---
+++ src/main/java/edu/cmu/mdnsim/nodes/RelayNode.java
@@ -0,0 +1,42 @@
+package edu.cmu.mdnsim.nodes;
+
+import java.net.UnknownHostException;
+
+import edu.cmu.mdnsim.config.Flow;
+import edu.cmu.mdnsim.config.Stream;
+
+/**
+ *
+ */
+public class RelayNode extends AbstractNode{
+
+	public RelayNode() throws UnknownHostException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void executeTask(Stream stream) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void terminateTask(Flow flow) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void releaseResource(Flow flow) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cleanUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
