4193662fd5a419cca4d35bac7a4085c0005b9c58
Jeremy Fu <gengf@andrew.cmu.edu>
2014-12-18 14:12:22 -0800

    Add rate correction to the SourceRunnable. When source node used nanoseconds, the accuracy of transfering high bit rates gets lower. Rate correction will dynamically adjusts sending rates to meet the requirement in work specification.

--- src/main/java/edu/cmu/mdnsim/nodes/SourceRunnable.java
+++ src/main/java/edu/cmu/mdnsim/nodes/SourceRunnable.java
@@ -53,9 +53,13 @@ class SourceRunnable extends NodeRunnable {
 		if(!initializeSocketAndPacket()){
 			return;
 		}
-
-		double packetPerSecond = rate / NodePacket.MAX_PACKET_LENGTH;
-		long nanosecondPerPacket = (long)(edu.cmu.mdnsim.nodes.AbstractNode.NANOSCONDS_PER_SECOND / packetPerSecond); 
+//
+//		//Target nanoseconds per packet
+//		long targetNspp = bps2nspp(rate);
+//		
+//		//Running nanoseconds per packet
+//		long runningNspp = targetNspp;
+		
 		
 		StreamReportMessage streamReportMessage = 
 				new StreamReportMessage.Builder(EventType.SEND_START, this.getDownStreamIds().iterator().next())
@@ -64,14 +68,23 @@ class SourceRunnable extends NodeRunnable {
 		this.sendStreamReport(streamReportMessage);
 
 		int packetId = 0;
-		System.err.println("[DELETE-JEREMY]SourceRunnable.run(): bytesToTransfer=" + bytesToTransfer);
+//		System.err.println("[DELETE-JEREMY]SourceRunnable.run(): bytesToTransfer=" + bytesToTransfer);
+		
+		
+		RateMonitor rateMonitor = new RateMonitor(rate);
+		rateMonitor.start();
+		
+		long runningNspp = rateMonitor.updateRunningNspp(-packet.getLength());
+		long expectedTime = System.nanoTime();
+		
 		while (bytesToTransfer > 0 && !isKilled()) {	
-			long begin = System.nanoTime();
+			
 
 			NodePacket nodePacket = 
-					bytesToTransfer <= NodePacket.MAX_PACKET_LENGTH ? 
-							new NodePacket(1, packetId, (int)bytesToTransfer) : new NodePacket(0, packetId);
+					bytesToTransfer <= NodePacket.MAX_PACKET_LENGTH ? new NodePacket(1, packetId, (int)bytesToTransfer) : new NodePacket(0, packetId);
+			
 			packet.setData(nodePacket.serialize());
+			
 			
 			try {
 				sendSocket.send(packet);
@@ -83,8 +96,9 @@ class SourceRunnable extends NodeRunnable {
 			bytesToTransfer -= packet.getLength();
 			setTotalBytesTranfered(getTotalBytesTranfered() + packet.getLength());
 
-			long end = System.nanoTime();
-			long nanosecondsRemaining = nanosecondPerPacket - (end - begin);
+			expectedTime += rateMonitor.updateRunningNspp(packet.getLength());
+			long cTime = System.nanoTime();
+			long nanosecondsRemaining = (expectedTime - cTime);
 			if (nanosecondsRemaining > 0) {
 				try {
 					long millisec = nanosecondsRemaining / 1000000;
@@ -94,6 +108,8 @@ class SourceRunnable extends NodeRunnable {
 					logger.error(e.toString());
 				}
 			}
+			
+			
 			packetId++;
 		}
 		/*
@@ -168,5 +184,74 @@ class SourceRunnable extends NodeRunnable {
 		} catch (MessageBusException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Convert the rate in bits per second to nanoseconds per packet
+	 * 
+	 * @param rate The sending rate in unit bps
+	 * 
+	 * @return
+	 */
+	private static long bps2nspp(int rate) {
+		double packetsPerSecond = rate / NodePacket.MAX_PACKET_LENGTH;
+		return (long)(edu.cmu.mdnsim.nodes.AbstractNode.NANOSCONDS_PER_SECOND / packetsPerSecond);	
+	}
+	
+	
+	private class RateMonitor {
+		
+		private long lastSecondTimestmp;
+		private long lastSecondSentBytes;
+
+		private long targetRate;
+		private long runningRate;
+		
+		private long offset;
+
+		
+		private long cumulativeDeficit = 0;
+		private long nspp;
+		
+		public RateMonitor(int targetRate) {
+			this.targetRate = targetRate;
+			this.runningRate = this.targetRate;
+		}
+		
+		public void start() {
+			lastSecondTimestmp = System.currentTimeMillis();
+			lastSecondSentBytes = 0;
+			nspp = bps2nspp((int)runningRate);
+		}
+		
+		public long updateRunningNspp(long packetLength) {
+			
+			lastSecondSentBytes += packetLength;
+			
+			long currentTimeMillis = System.currentTimeMillis();
+			
+			if ((currentTimeMillis - lastSecondTimestmp) > 1000) {
+				
+				long lastSecondRate = lastSecondSentBytes / (currentTimeMillis - lastSecondTimestmp) * 1000;				
+
+				long cDeficit = targetRate - lastSecondRate;
+				cumulativeDeficit += cDeficit;
+				
+				
+				offset = Math.max(-targetRate, cumulativeDeficit);
+				
+				System.out.println(String.format("RateMonitor.updateRunningNspp(): target:%d, running:%d, observed:%d, offset:%d, cumulativeOffset:%d", targetRate, runningRate, lastSecondRate, offset, this.cumulativeDeficit));
+				runningRate = targetRate + offset;
+				
+				lastSecondSentBytes = 0;
+				lastSecondTimestmp = System.currentTimeMillis();
+				nspp = bps2nspp((int)runningRate);
+				
+			}
+			
+			
+			return nspp;
+		}
+		
 	}
 }
