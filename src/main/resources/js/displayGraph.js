6d5e6ccc3ce945abc647b5b3509114d0f7cf8c8f
Jeremy Fu <gengf@andrew.cmu.edu>
2015-05-01 09:21:53 -0700

    Add topology generator to the frontend

--- src/main/resources/js/displayGraph.js
+++ src/main/resources/js/displayGraph.js
@@ -7,6 +7,10 @@ var s = null;
 //Represents the camera object used by sigma library
 var cam = null;
 
+var streamCounter = 1;
+
+var dragListener;
+
 var clickNode = false;
 var nodeIdOnWatcher = null;
 
@@ -16,7 +20,7 @@ var edgeIdOnWatcher = null;
 function createGraph(initial_data){
 	
 	console.log('createGraph called', initial_data);
-
+	sigma.renderers.def = sigma.renderers.canvas;
 	s = new sigma({
 		  graph: initial_data,
 		  renderer: {
@@ -498,7 +502,124 @@ $(document).ready(function() {
 		console.log('Reset wsinput');
 		return false;
 	});
+	
+	$("#btnAddStream").click(function(e) {
+		
+		var createNode = function(nodeType, nodeId, upstreamId) {
+			var node = {};
+			node.NodeType = nodeType;
+			node.NodeId   = nodeId;
+			node.UpstreamId = upstreamId;
+			return node;
+		}
 
+		var fn = function(streamId) {
+
+			var source = "mdnsim-node" + Math.ceil(Math.random() * 20) + ":source";
+
+			var relay1 = "mdnsim-node" + Math.ceil(Math.random() * 20) + ":relay1";
+			
+			//Tier 2 relays num: 2 ~ 3
+			var relay2Hash = {};
+			var relay2Pool = [];
+			var relay2Num = Math.ceil(Math.random() * 2 + 1);
+			for(var i = 0; i < relay2Num; i++) {
+				var tmp = Math.ceil(Math.random()*20);
+				while(relay2Hash[tmp] === true) {
+				 	tmp = Math.ceil(Math.random()*20);
+				}
+				relay2Hash[tmp] = true;
+			}
+			for (var val in relay2Hash) {
+				relay2Pool.push("mdnsim-node" + val + ":relay2");
+			}
+
+			//Tier 3 Relay Tag: tier 2 num  + 1 ~ 3
+			var relay3Hash = {};
+			var relay3Pool = [];
+			var relay3Num = Math.ceil(Math.random() * 3 + relay2Num);
+			for(var i = 0; i < relay3Num; i++) {
+				var tmp = Math.ceil(Math.random()*20);
+				while(relay3Hash[tmp] === true) {
+				 	tmp = Math.ceil(Math.random()*20);
+				}
+				relay3Hash[tmp] = true;
+			}
+			for (var val in relay3Hash) {
+				relay3Pool.push("mdnsim-node" + val + ":relay3");
+			}
+
+
+			var graph = {};
+			graph.upstream = {};
+
+			for (var i = 1; i <= 20; i++) {
+				var sink = "mdnsim-node" + i + ":sink";
+				if (i < relay3Num) {
+					graph.upstream[sink] = relay3Pool[i];
+				} else {
+					var randomIdx = Math.floor(Math.random() * (relay3Num));
+					
+					console.log(sink + "is attached to " + randomIdx);
+					
+					graph.upstream[sink] = relay3Pool[randomIdx];
+				}
+			}
+
+			for (var i = 0; i < relay3Num; i++) {
+				var relay3 = relay3Pool[i];
+				if (i < relay2Num) {
+					graph.upstream[relay3] = relay2Pool[i];
+				} else {
+					var randomIdx = Math.floor(Math.random() * (relay2Num));
+					graph.upstream[relay3] = relay2Pool[randomIdx];
+				}
+			}
+
+			for (var i = 0; i < relay2Num; i++) {
+				var relay2 = relay2Pool[i];
+				graph.upstream[relay2] = relay1;
+			}
+
+			graph.upstream[relay1] = source;
+
+
+			foo = {
+				SimId:"demo",
+				StreamList:[],
+
+			}
+
+			var stream = {};
+			stream.StreamId = "Stream" + streamId;
+			stream.DataSize = "80000000";
+			stream.KiloBitRate = "5000";
+			stream.FlowList = [];
+
+			for (var i = 1; i <= 20; i++) {
+				
+				var flow = {};
+				flow.NodeList = [];
+				var sink = "mdnsim-node" + i + ":sink";
+				flow.NodeList.push(createNode("SinkNode", sink, graph.upstream[sink]));
+				var relay3 = graph.upstream[sink];
+				flow.NodeList.push(createNode("RelayNode", relay3, graph.upstream[relay3]));
+				var relay2 = graph.upstream[relay3];
+				flow.NodeList.push(createNode("RelayNode", relay2, graph.upstream[relay2]));
+				var relay1 = graph.upstream[relay2];
+				flow.NodeList.push(createNode("RelayNode", relay1, graph.upstream[relay1]));
+				var src = graph.upstream[relay1];
+				flow.NodeList.push(createNode("SourceNode", src, "NULL"));
+				stream.FlowList.push(flow);
+			}
+
+			foo.StreamList.push(stream);
+
+			loggerResource.message().method("POST").to("/mdnsim/work_config").data(foo).send();
+
+		};
+		fn("stream" + streamCounter++);
+	})
 	/*
 	 * When the input file element is reset when flow action is changed, the event handlers
 	 * for the input file element is lost. But, if we add the event handler on a parent 
